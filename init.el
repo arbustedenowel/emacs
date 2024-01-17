a8226e91dbe4ea8423a8fd7e46a9fe67e8f3537f
victorien motte <victorien.motte.etu@univ.lille-fr>
2024-01-17 10:41:26 +0100

    fixed issue while bootstraping use-package

--- init.el
+++ init.el
@@ -36,8 +36,26 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(require 'use-package)
+
+;;bootstrap use package 
+;;(require 'use-package)
+;;(setq use-package-always-ensure t)
+
+(eval-after-load 'gnutls
+  '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
 (setq use-package-always-ensure t)
+
+
+
+
+
+
 
 (use-package command-log-mode)
 
