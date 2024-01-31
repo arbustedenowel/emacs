20b87f96bfbb0ed4e34e7ffe18bffcd941b5ac95
victorien <vic.mot59@gmail.com>
2024-01-31 22:58:33 +0100

    added a wotkaround caused by use-package

--- init.el
+++ init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file (expand-file-name "~/.config/emacs/myInit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company which-key treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil rainbow-delimiters pdf-tools org-bullets helm exwm ef-themes doom-themes doom-modeline dashboard counsel command-log-mode all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
