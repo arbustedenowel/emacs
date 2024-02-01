d824bae7e8eb27ecb9f74d8fac4a04b1ab5750b7
victorien motte <vic.mot59@gmail.com>
2024-02-01 11:10:06 +0100

    added vterm and popper

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
  '(package-selected-packages
    '(company which-key treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil rainbow-delimiters pdf-tools org-bullets helm exwm ef-themes doom-themes doom-modeline dashboard counsel command-log-mode all-the-icons)))
 (custom-set-faces
